b1852918b8e61782f6e73a9ea954f8fa2593eeae
potifersan <potifersan@gmail.com>
2022-12-14 06:29:50 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,13 @@
 #include "main.h"
-
+#include <unistd.h>
 /**
- * main - Prints Holberton as a message
+ * _putchar - writes thr character c to stdout
+ * @c: the character to print
  *
- * Return: Always 0 (Success)
+ * Return: on success 1
  */
 
-int main(void)
+int _putchar (char c)
 {
-	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-	int count, sz;
-
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count++)
-	{
-		_putchar(str[count]);
-	}
-	_putchat('\n');
-	return (0);
+	return (write (1, &c, 1));
 }
