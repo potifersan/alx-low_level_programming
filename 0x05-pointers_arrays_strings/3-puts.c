15c4cd9b7943b1f79a5d87c89554713ea12f6244
potifersan <potifersan@gmail.com>
2022-12-19 13:48:47 -0800

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+nclude "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - To print the value of *str from main
+ * @str: string pointer.
+ * Printing with putchar
+ */
+
+void _puts(char *str)
+{
+	int c;
+
+	for (c = 0; str[c] != '\0'; c++)
+	{
+		putchar(str[c]);
+	}
+	putchar ('\n');
+}
