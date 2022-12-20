49d4ff8f7fc0f346ba639bca1661ce1ce759e7ba
potifersan <potifersan@gmail.com>
2022-12-20 13:02:43 -0800

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -22,26 +22,25 @@ int _atoi(char *s)
 
 	while (i < len && m == 0)
 	{
-		if (s[i] == '-')
-			++j;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			tmp = s[i] - '0';
-
-if (j % 2);
-			tmp = -tmp;
-			k = k * 10 + tmp;
-			m = 1;
-
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			m = 0;
+			if (s[i] == '-')
+				++j;
+	
+			if (s[i] >= '0' && s[i] <= '9')
+			{
+						tmp = s[i] - '0';
+						if (j % 2)
+							tmp = -tmp;
+						k = k * 10 + tmp;
+						m = 1;
+						if (s[i + 1] < '0' || s[i + 1] > '9')
+							break;
+						m = 0;
+					}
+			i++;
 		}
-		i++;
-	}
 
 	if (m == 0)
-		return (0);
+	return (0);
+
 	return (k);
 }
