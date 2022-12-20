04835ca29f8aa3f6ff7f4aac5cc8db6aa8c6bea0
potifersan <potifersan@gmail.com>
2022-12-20 06:46:13 -0800

    string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - Reversing string
+ * @s: Being the pointer
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int a, b, c;
+	char m;
+
+	for (a = 0; s[a] != '\0'; a++)
+		;
+
+	c = a
+		for (a--, b = 0; b < c / 2; a--, b++)
+		{
+			m = s[b];
+			s[b] = s[a];
+			s[a] = m;
+		}
+}
